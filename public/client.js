2136131fcdf30a6b1831b1bf676c60bec3c711cb
jon-wade <jonwadeuk@gmail.com>
2016-05-28 09:53:11 +0100

    remove the # from the URL

--- public/client.js
+++ public/client.js
@@ -1,10 +1,12 @@
 var client = angular.module('client', ['ngRoute']);
 
-client.config(function($routeProvider) {
+client.config(function($routeProvider, $locationProvider) {
     $routeProvider.when('/', {
         templateUrl: './home/home.html',
         controller: 'homeCtrl'
     });
+
+    $locationProvider.html5Mode(true);
 });
 
 client.controller('homeCtrl', [function(){
